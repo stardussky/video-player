fac7aef5348297055a17a467cb6c6e522dcaa7f2
Evan <evan@blockstudio.tw>
2021-10-17 19:38:11 +0800

    [feat] svgIcon

---
+++ src/components/SvgIcon/SvgIcon.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './svg-icon.scss'
+
+function SvgIcon (props) {
+    const { name } = props
+    const symbolId = `#${name}`
+
+    return (
+        <svg
+            {...props}
+            className='svg-icon'
+            aria-hidden='true'
+        >
+            <use href={symbolId} />
+        </svg>
+    )
+}
+
+SvgIcon.propTypes = {
+    name: PropTypes.string,
+}
+
+export default SvgIcon
