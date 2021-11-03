b0d1d7d52f931d49a56b152311b85e44b1dbd8af
Evan <evan@blockstudio.tw>
2021-11-03 23:45:44 +0800

    [feat] 新增 playback rate組件，playback rate 功能

---
+++ src/components/VideoPlaybackRate/VideoPlaybackRate.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import './video-playback-rate.scss'
+
+const VideoPlaybackRate = ({ onBack, transformPlaybackRate, playbackRate, onPlaybackChange }) => {
+    const rate = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2]
+    return (
+        <ul className='video-playback-rate'>
+            <li className='video-setting__item' onClick={() => onBack(null)}>
+                <div className='video-setting__item-option video-setting__item-display'>
+                    <p>播放速度</p>
+                    <p>{transformPlaybackRate}</p>
+                </div>
+            </li>
+            {
+                rate.map(value => (
+                    <li
+                        className='video-setting__item'
+                        key={value}
+                        onClick={() => onPlaybackChange(value)}
+                    >
+                        <div className='video-setting__item-option'>
+                            {value}
+                        </div>
+                    </li>
+                ))
+            }
+            <li>
+                <div
+                    className='video-playback-rate__range video-setting__item-option'
+                    style={{
+                        '--progress': `${(playbackRate - 0.125) / 2 * 100}%`,
+                    }}
+                >
+                    <input
+                        type='range'
+                        min='0.25'
+                        max='2'
+                        step='0.01'
+                        value={playbackRate}
+                        onChange={(e) => onPlaybackChange(Number(e.target.value))}
+                    />
+                </div>
+            </li>
+        </ul>
+    )
+}
+
+VideoPlaybackRate.propTypes = {
+    onBack: PropTypes.func,
+    transformPlaybackRate: PropTypes.string,
+    playbackRate: PropTypes.number,
+    onPlaybackChange: PropTypes.func,
+}
+
+export default VideoPlaybackRate
